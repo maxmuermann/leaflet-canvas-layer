c51d204700efe9d3f57532d913d6819b4fd518a1
Max Muermann <max@muermann.org>
2016-02-05 11:12:50 +1100

    Make canvas layer extend L.Layer

--- leaflet_canvas_layer.js
+++ leaflet_canvas_layer.js
@@ -3,7 +3,7 @@ if(typeof(L) !== 'undefined') {
  * full canvas layer implementation for Leaflet
  */
 
-L.CanvasLayer = L.Class.extend({
+L.CanvasLayer = L.Layer.extend({
 
   includes: [L.Mixin.Events, L.Mixin.TileLoader],
 
